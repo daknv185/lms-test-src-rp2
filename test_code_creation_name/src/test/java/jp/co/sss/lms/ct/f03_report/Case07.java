1b30bfe14529ca5dc588d34c092ca200138b7dfa
t-yoshida <daknv.its.202507@gmail.com>
2025-11-21 18:12:54 +0900

    add:Case07 受講生レポート新規登録(日報)の動作確認テストコード追加
    WebElementを利用し、日報が正しく新規登録されるか確認するテストコードを作成しました。
    
    以下、期待値
    テスト1: ログイン画面が表示される
    
    テスト2 : 画面遷移後、コース詳細画面が表示される
    
    テスト3 : 画面遷移後、セクション詳細画面が表示され、画面左下のボタン名が「日報【デモ】を提出する」になっている
    
    テスト4 : 画面遷移後、レポート登録画面が表示される
    
    テスト5 : 画面遷移後、セクション詳細画面が表示される。
    その後、セクション詳細画面左下部のボタンが「提出済み日報【デモ】を確認する」に更新されている
    --
    add:Case08 受講生のレポート修正(週報) が正常に動作するか確認するテストケース追加
    
    WebElementを利用し、週報が正常に修正されるか確認するテストを作成しました。
    
    以下、期待値
    テスト1: ログイン画面が表示される
    
    テスト2 : 画面遷移後、コース詳細画面が表示される
    
    テスト3 : 画面遷移後、セクション詳細画面が表示される。画面左下のボタン名が「提出済み週報【デモ】を提出する」となっている。
    
    テスト4 : 画面遷移後、レポート登録画面が表示される
    
    テスト5 : 画面遷移後、セクション詳細画面が表示される。
    
    テスト6 : 画面遷移後、ユーザー詳細画面に遷移する
    
    テスト7 : レポート詳細画面へ遷移し、「所感」欄にテスト5での入力値が反映される

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,8 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +11,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -36,6 +40,12 @@ public class Case07 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/");
 	}
 
 	@Test
@@ -43,6 +53,26 @@ public class Case07 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+		String lmsId = "StudentAA01";
+		String lmsPass = "StudentAA0";
+
+		loginId.clear();
+		loginId.sendKeys(lmsId);
+		password.clear();
+		password.sendKeys(lmsPass);
+
+		getEvidence(new Object() {}, "01_beforeLogin");
+
+		login.click();
+
+		getEvidence(new Object() {}, "02_afterLogin");
+
+		String screenInfo = webDriver.findElement(By.className("active")).getText();
+
+		assertTrue(screenInfo.contains("コース詳細"));
 	}
 
 	@Test
@@ -50,6 +80,18 @@ public class Case07 {
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		
+		scrollBy("300");
+		WebElement record = webDriver.findElement(By.xpath("//table//tr[td//span[text()='未提出']]"));
+
+		record.findElement(By.cssSelector("input[type='submit']")).click();
+		
+		getEvidence(new Object() {});
+		
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/section/detail");
+		
+		
 	}
 
 	@Test
@@ -57,13 +99,43 @@ public class Case07 {
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
 		// TODO ここに追加
-	}
+		WebElement applicationBtn = webDriver
+				.findElement(By.xpath("//*[@id=\"sectionDetail\"]/table/tbody/tr[2]/td/form/input[5]"));
+		applicationBtn.click();
+		
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/report/regist");
+		
+		getEvidence(new Object() {});
+}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
 		// TODO ここに追加
+		WebElement textForm = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[2]/fieldset/div/div/button"));
+
+		String text = "test";
+		
+		textForm.clear();
+		textForm.sendKeys(text);
+		
+		getEvidence(new Object() {}, "01_textInputted");
+		
+		submitBtn.click();
+		
+		//以下、セクション詳細画面
+		visibilityTimeout(By.xpath("//*[@id=\"sectionDetail\"]/table/tbody/tr[2]/td/form/input[6]"),5);
+		
+		WebElement dayRepSubmitBtn = webDriver.findElement(By.xpath("//*[@id=\"sectionDetail\"]/table/tbody/tr[2]/td/form/input[6]"));
+		
+		String reportSubmitBtn = dayRepSubmitBtn.getAttribute("value");
+		
+		getEvidence(new Object() {}, "02_submitBtnChanged");
+		assertEquals("提出済み日報【デモ】を確認する",reportSubmitBtn);
+		
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
@@ -1,14 +1,17 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -25,17 +28,23 @@ public class Case08 {
 		createDriver();
 	}
 
-	/** 後処理 */
-	@AfterAll
-	static void after() {
-		closeDriver();
-	}
+//	/** 後処理 */
+//	@AfterAll
+//	static void after() {
+//		closeDriver();
+//	}
 
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/");
 	}
 
 	@Test
@@ -43,6 +52,26 @@ public class Case08 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+		String lmsId = "StudentAA01";
+		String lmsPass = "StudentAA0";
+
+		loginId.clear();
+		loginId.sendKeys(lmsId);
+		password.clear();
+		password.sendKeys(lmsPass);
+
+		getEvidence(new Object() {}, "01_beforeLogin");
+
+		login.click();
+
+		getEvidence(new Object() {}, "02_afterLogin");
+
+		String screenInfo = webDriver.findElement(By.className("active")).getText();
+
+		assertTrue(screenInfo.contains("コース詳細"));
 	}
 
 	@Test
@@ -50,6 +79,15 @@ public class Case08 {
 	@DisplayName("テスト03 提出済の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		
+		WebElement record = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div[2]/div[2]/table/tbody/tr[2]/td[5]/form/input[3]"));
+		
+		record.click();
+		
+		getEvidence(new Object() {});
+		
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/section/detail");
 	}
 
 	@Test
@@ -57,6 +95,17 @@ public class Case08 {
 	@DisplayName("テスト04 「確認する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
 		// TODO ここに追加
+		WebElement applicationBtn = webDriver
+				.findElement(By.xpath("//*[@id=\"sectionDetail\"]/table[2]/tbody/tr[3]/td/form/input[6]"));
+		
+		scrollBy("200");
+		
+		applicationBtn.click();
+		
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/report/regist");
+		
+		getEvidence(new Object() {});
 	}
 
 	@Test
@@ -64,6 +113,30 @@ public class Case08 {
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しセクション詳細画面に遷移")
 	void test05() {
 		// TODO ここに追加
+		WebElement textForm = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+
+		String text = "This is test";
+		
+		textForm.clear();
+		textForm.sendKeys(text);
+		
+		getEvidence(new Object() {}, "01_textInputted");
+		
+		scrollBy("200");
+		
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		submitBtn.click();
+		
+		//以下、セクション詳細画面
+		visibilityTimeout(By.xpath("//*[@id=\"sectionDetail\"]/table/tbody/tr[2]/td/form/input[6]"),5);
+		
+		WebElement dayRepSubmitBtn = webDriver.findElement(By.xpath("//*[@id=\"sectionDetail\"]/table[2]/tbody/tr[3]/td/form/input[6]"));
+		
+		String reportSubmitBtn = dayRepSubmitBtn.getAttribute("value");
+		
+		getEvidence(new Object() {});
+		assertEquals("提出済み週報【デモ】を確認する",reportSubmitBtn);
 	}
 
 	@Test
@@ -71,6 +144,15 @@ public class Case08 {
 	@DisplayName("テスト06 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test06() {
 		// TODO ここに追加
+		WebElement profileLink = webDriver.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[2]/li[2]/a"));
+		
+		profileLink.click();
+		
+		getEvidence(new Object() {});
+		
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/user/detail");
+		
 	}
 
 	@Test
@@ -78,6 +160,24 @@ public class Case08 {
 	@DisplayName("テスト07 該当レポートの「詳細」ボタンを押下しレポート詳細画面で修正内容が反映される")
 	void test07() {
 		// TODO ここに追加
+		String inputtedText = "This is test";
+		WebElement profileLink = webDriver.findElement(By.xpath("//*[@id=\"main\"]/table[3]/tbody/tr[9]/td[5]/form[1]/input[1]"));
+	
+		scrollBy("1000");
+		
+		profileLink.click();
+		
+		getEvidence(new Object() {});
+		
+		//レポート詳細画面へ遷移できているかの確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/report/detail");
+		
+		//入力内容が反映されているかの確認
+		WebElement thoughts = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/table/tbody/tr[2]/td"));
+		String actualText = thoughts.getText();
+		
+		assertEquals(inputtedText,actualText);
 	}
 
 }
