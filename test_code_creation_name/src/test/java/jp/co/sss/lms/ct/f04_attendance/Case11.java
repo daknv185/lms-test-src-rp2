947d2a9c91827d60171c5f6db0804ea033fdc353
t-yoshida <daknv.its.202507@gmail.com>
2025-11-25 17:44:23 +0900

    add:Case11 受講生 勤怠直接編集の動作確認テストコード追加
    WebElementを利用し、勤怠直接編集が適切に動作するか確認するテストを作成しました。
    
    以下、期待値
    テスト1: ログイン画面が表示される
    
    テスト2 : 画面遷移後、コース詳細画面が表示される
    
    テスト3 : 画面遷移後、勤怠管理画面が表示される。
    
    テスト4 : 画面遷移後、勤怠情報直接編集画面が表示される
    
    テスト5 : 画面遷移後、勤怠画面が表示される。勤怠情報直接編集画面で入力した定時(出勤09:00、退勤18:00)が全ての日付に正しく入力されている。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
@@ -1,6 +1,10 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +13,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -36,6 +44,12 @@ public class Case11 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/");
 	}
 
 	@Test
@@ -43,6 +57,28 @@ public class Case11 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+		String lmsId = "StudentAA01";
+		String lmsPass = "StudentAA0";
+
+		loginId.clear();
+		loginId.sendKeys(lmsId);
+		password.clear();
+		password.sendKeys(lmsPass);
+
+		getEvidence(new Object() {
+		}, "01_beforeLogin");
+
+		login.click();
+
+		getEvidence(new Object() {
+		}, "02_afterLogin");
+
+		String screenInfo = webDriver.findElement(By.className("active")).getText();
+
+		assertTrue(screenInfo.contains("コース詳細"));
 	}
 
 	@Test
@@ -50,6 +86,24 @@ public class Case11 {
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		visibilityTimeout(By.xpath("//*[@id=\"nav-content\"]/ul[1]/li[3]/a"), 5);
+		WebElement attendance = webDriver.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[1]/li[3]/a"));
+
+		attendance.click();
+
+		try {
+			Alert alert = webDriver.switchTo().alert();
+			alert.accept();
+		} catch (NoAlertPresentException ignore) {
+
+		}
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/attendance/detail");
+
 	}
 
 	@Test
@@ -57,6 +111,17 @@ public class Case11 {
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
 		// TODO ここに追加
+		visibilityTimeout(By.xpath("//*[@id=\"main\"]/div[1]/p/a"), 5);
+		WebElement editAttendanceLink = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/p/a"));
+
+		editAttendanceLink.click();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/attendance/update");
+
 	}
 
 	@Test
@@ -64,6 +129,42 @@ public class Case11 {
 	@DisplayName("テスト05 すべての研修日程の勤怠情報を正しく更新し勤怠管理画面に遷移")
 	void test05() {
 		// TODO ここに追加
-	}
+		WebElement table = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/table"));
+		WebElement updateBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/div/input"));
 
+		List<WebElement> regHourBtns = table
+				.findElements(By.cssSelector(".btn.btn-success.default-button"));
+
+		for (WebElement button : regHourBtns) {
+			button.click();
+			scrollBy("50");
+		}
+
+		getEvidence(new Object() {
+		}, "01_regHourInputted");
+
+		updateBtn.click();
+
+		Alert alert = webDriver.switchTo().alert();
+
+		alert.accept();
+
+		getEvidence(new Object() {
+		}, "02");
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/attendance/update");
+
+		List<WebElement> rows = webDriver.findElements(By.cssSelector("table#main tbody tr"));
+
+		for (WebElement row : rows) {
+			List<WebElement> cells = row.findElements(By.cssSelector("td.w80"));
+			String start = cells.get(0).getText(); // 0列目が出勤
+			String end = cells.get(1).getText(); // 1列目が退勤
+
+			assertEquals("09:00", start);
+			assertEquals("18:00", end);
+		}
+
+	}
 }
