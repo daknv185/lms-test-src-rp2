3aaa408c30a0c45dd0bae69633583a2992aaaf6d
t-yoshida <daknv.its.202507@gmail.com>
2025-11-26 17:24:47 +0900

    add:Case09 受講生のレポート登録における入力チェックの動作確認テストコード追加
    WebElementを利用し、レポート登録における入力チェックが適切に動作するか確認するテストを作成しました。
    
    以下、期待値
    テスト1: ログイン画面が表示される
    
    テスト2 : 画面遷移後、コース詳細画面が表示される
    
    テスト3 : 画面遷移後、ユーザー詳細画面に遷移する
    
    テスト4 : 画面遷移後、レポート登録画面に遷移する。
    
    テスト5 : 学習項目フォームの下に「理解度を入力した場合は、学習項目は必須です。」とエラーメッセージが出力される。
    
    テスト6 : 理解度フォームの下に「学習項目を入力した場合は、理解度は必須です。」とエラーメッセージが出力される。
    
    テスト7 : 目標の達成度フォームの下に「目標の達成度は半角数字で入力してください」とエラーメッセージが出力される。
    
    テスト8 : 目標の達成度フォームの下に「目標の達成度は1～10の範囲内で入力してください」とエラーメッセージが出力される。
    
    テスト9 : 目標の達成度フォームの下に「目標の達成度は必須です」とエラーメッセージが出力される。
    
    テスト10 : 目標の所感フォームの下に「所感は必須です」とエラーメッセージが出力される。
    
    テスト11 : 所感フォームの下に「所感の長さが最大値(2000)を超えています」とエラーメッセージが出力される。
    
    テスト12 : 一週間の振り返りフォームの下に「一週間の振り返りの長さが最大値(2000)を超えています」とエラーメッセージが出力される。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case09.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case09.java
@@ -1,6 +1,8 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +11,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -36,6 +40,12 @@ public class Case09 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/");
 	}
 
 	@Test
@@ -43,6 +53,28 @@ public class Case09 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+		String lmsId = "StudentAA01";
+		String lmsPass = "StudentAA0";
+
+		loginId.clear();
+		loginId.sendKeys(lmsId);
+		password.clear();
+		password.sendKeys(lmsPass);
+
+		getEvidence(new Object() {
+		}, "01_beforeLogin");
+
+		login.click();
+
+		getEvidence(new Object() {
+		}, "02_afterLogin");
+
+		String screenInfo = webDriver.findElement(By.className("active")).getText();
+
+		assertTrue(screenInfo.contains("コース詳細"));
 	}
 
 	@Test
@@ -50,6 +82,15 @@ public class Case09 {
 	@DisplayName("テスト03 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		WebElement profileLink = webDriver.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[2]/li[2]/a"));
+
+		profileLink.click();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/user/detail");
 	}
 
 	@Test
@@ -57,6 +98,16 @@ public class Case09 {
 	@DisplayName("テスト04 該当レポートの「修正する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
 		// TODO ここに追加
+		scrollBy("1000");
+		
+		WebElement fix = webDriver.findElement(By.xpath("//*[@id=\"main\"]/table[3]/tbody/tr[2]/td[5]/form[2]/input[1]"));
+		fix.click();
+		
+		getEvidence(new Object() {});
+		
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/report/regist");
+		
 	}
 
 	@Test
@@ -64,6 +115,42 @@ public class Case09 {
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しエラー表示：学習項目が未入力")
 	void test05() {
 		// TODO ここに追加
+		WebElement contents = webDriver.findElement(By.xpath("//*[@id=\"intFieldName_0\"]"));
+		WebElement understanding = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]"));
+		WebElement attainmentLev = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement Impressions = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		final String testWord = "test"; 
+		
+		
+		contents.clear();
+		
+		understanding.click();
+		
+		WebElement understandingLev1 = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]/option[2]"));
+		
+		understandingLev1.click();
+		
+		attainmentLev.clear();
+		attainmentLev.sendKeys("1");
+		
+		getEvidence(new Object() {},"01_beforeSubmit");
+		
+		Impressions.clear();
+		Impressions.sendKeys(testWord);
+		
+		getEvidence(new Object() {},"02_beforeSubmit");
+		
+		scrollBy("1000");
+		
+		submitBtn.click();
+		
+		getEvidence(new Object() {},"03_afterSubmit");
+		
+		String error = webDriver.findElement(By.className("error")).getText();
+
+		assertEquals(error,"理解度を入力した場合は、学習項目は必須です。");
 	}
 
 	@Test
@@ -71,6 +158,43 @@ public class Case09 {
 	@DisplayName("テスト06 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：理解度が未入力")
 	void test06() {
 		// TODO ここに追加
+		WebElement contents = webDriver.findElement(By.xpath("//*[@id=\"intFieldName_0\"]"));
+		WebElement understanding = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]"));
+		WebElement attainmentLev = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement Impressions = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		final String testWord = "test"; 
+		
+		
+		contents.clear();
+		contents.sendKeys(testWord);
+		
+		understanding.click();
+		
+		WebElement understandingLev1 = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]/option[1]"));
+		
+		understandingLev1.click();
+		
+		attainmentLev.clear();
+		attainmentLev.sendKeys("1");
+		
+		getEvidence(new Object() {},"01_beforeSubmit");
+		
+		Impressions.clear();
+		Impressions.sendKeys(testWord);
+		
+		getEvidence(new Object() {},"02_beforeSubmit");
+		
+		scrollBy("1000");
+		
+		submitBtn.click();
+		
+		getEvidence(new Object() {},"03_afterSubmit");
+		
+		String error = webDriver.findElement(By.className("error")).getText();
+
+		assertEquals(error,"学習項目を入力した場合は、理解度は必須です。");
 	}
 
 	@Test
@@ -78,6 +202,47 @@ public class Case09 {
 	@DisplayName("テスト07 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度が数値以外")
 	void test07() {
 		// TODO ここに追加
+		WebElement contents = webDriver.findElement(By.xpath("//*[@id=\"intFieldName_0\"]"));
+		WebElement understanding = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]"));
+		WebElement attainmentLev = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement Impressions = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		final String testWord = "test"; 
+		
+		
+		contents.clear();
+		contents.sendKeys(testWord);
+		
+		understanding.click();
+		
+		WebElement understandingLev1 = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]/option[2]"));
+		
+		understandingLev1.click();
+		
+		attainmentLev.clear();
+		attainmentLev.sendKeys(testWord);
+		
+		getEvidence(new Object() {},"01_beforeSubmit");
+		
+		Impressions.clear();
+		Impressions.sendKeys(testWord);
+		
+		getEvidence(new Object() {},"02_beforeSubmit");
+		
+		scrollBy("1000");
+		
+		submitBtn.click();
+		
+		scrollBy("100");
+		
+		getEvidence(new Object() {},"03_afterSubmit");
+		
+		String error = webDriver.findElement(By.className("error")).getText();
+
+		assertEquals(error,"目標の達成度は半角数字で入力してください");
+		
+		scrollTo("0");
 	}
 
 	@Test
@@ -85,20 +250,244 @@ public class Case09 {
 	@DisplayName("テスト08 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度が範囲外")
 	void test08() {
 		// TODO ここに追加
+		WebElement contents = webDriver.findElement(By.xpath("//*[@id=\"intFieldName_0\"]"));
+		WebElement understanding = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]"));
+		WebElement attainmentLev = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement Impressions = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		final String testWord = "test"; 
+		
+		
+		contents.clear();
+		contents.sendKeys(testWord);
+		
+		understanding.click();
+		
+		WebElement understandingLev1 = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]/option[2]"));
+		
+		understandingLev1.click();
+		
+		attainmentLev.clear();
+		attainmentLev.sendKeys("11");
+		
+		getEvidence(new Object() {},"01_beforeSubmit");
+		
+		Impressions.clear();
+		Impressions.sendKeys(testWord);
+		
+		getEvidence(new Object() {},"02_beforeSubmit");
+		
+		scrollBy("1000");
+		
+		submitBtn.click();
+		
+		scrollBy("100");
+		
+		getEvidence(new Object() {},"03_afterSubmit");
+		
+		String error = webDriver.findElement(By.className("error")).getText();
+
+		assertEquals(error,"目標の達成度は1～10の範囲内で入力してください");
+		
+		scrollTo("0");
 	}
 
 	@Test
 	@Order(9)
-	@DisplayName("テスト09 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度・所感が未入力")
+	@DisplayName("テスト09 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度が未入力")
 	void test09() {
 		// TODO ここに追加
+		WebElement contents = webDriver.findElement(By.xpath("//*[@id=\"intFieldName_0\"]"));
+		WebElement understanding = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]"));
+		WebElement attainmentLev = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement Impressions = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		final String testWord = "test"; 
+		
+		
+		contents.clear();
+		contents.sendKeys(testWord);
+		
+		understanding.click();
+		
+		WebElement understandingLev1 = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]/option[2]"));
+		
+		understandingLev1.click();
+		
+		attainmentLev.clear();
+		
+		getEvidence(new Object() {},"01_beforeSubmit");
+		
+		Impressions.clear();
+		Impressions.sendKeys(testWord);
+		
+		getEvidence(new Object() {},"02_beforeSubmit");
+		
+		scrollBy("1000");
+		
+		submitBtn.click();
+		
+		scrollBy("100");
+		
+		getEvidence(new Object() {},"03_afterSubmit");
+		
+		String error = webDriver.findElement(By.className("error")).getText();
+
+		assertEquals(error,"目標の達成度は必須です");
+		
+		scrollTo("0");
+		
 	}
 
 	@Test
 	@Order(10)
-	@DisplayName("テスト10 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：所感・一週間の振り返りが2000文字超")
+	@DisplayName("テスト10 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：所感が未入力")
 	void test10() {
 		// TODO ここに追加
+		WebElement contents = webDriver.findElement(By.xpath("//*[@id=\"intFieldName_0\"]"));
+		WebElement understanding = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]"));
+		WebElement attainmentLev = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement Impressions = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		final String testWord = "test"; 
+		
+		
+		contents.clear();
+		contents.sendKeys(testWord);
+		
+		understanding.click();
+		
+		WebElement understandingLev1 = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]/option[2]"));
+		
+		understandingLev1.click();
+		
+		attainmentLev.clear();
+		attainmentLev.sendKeys("1");
+		
+		getEvidence(new Object() {},"01_beforeSubmit");
+		
+		Impressions.clear();
+		
+		getEvidence(new Object() {},"02_beforeSubmit");
+		
+		scrollBy("1000");
+		
+		submitBtn.click();
+		
+		scrollBy("250");
+		
+		getEvidence(new Object() {},"03_afterSubmit");
+		
+		String error = webDriver.findElement(By.className("error")).getText();
+
+		assertEquals(error,"所感は必須です");
+		
+		scrollTo("0");
+	}
+
+	@Test
+	@Order(11)
+	@DisplayName("テスト11 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：所感が2000文字超")
+	void test11() {
+		// TODO ここに追加
+		WebElement contents = webDriver.findElement(By.xpath("//*[@id=\"intFieldName_0\"]"));
+		WebElement understanding = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]"));
+		WebElement attainmentLev = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement Impressions = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		final String testWord = "test"; 
+		
+		
+		contents.clear();
+		contents.sendKeys(testWord);
+		
+		understanding.click();
+		
+		WebElement understandingLev1 = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]/option[2]"));
+		
+		understandingLev1.click();
+		
+		attainmentLev.clear();
+		attainmentLev.sendKeys("1");
+		
+		getEvidence(new Object() {},"01_beforeSubmit");
+		
+		Impressions.clear();
+		for(int i = 0; i <= 91 ; i++) {
+			Impressions.sendKeys("あいうえおかきくけこさしすせそたちつてとなに");
+		}
+		
+		getEvidence(new Object() {},"02_beforeSubmit");
+		
+		scrollBy("1000");
+		
+		submitBtn.click();
+		
+		scrollBy("250");
+		
+		getEvidence(new Object() {},"03_afterSubmit");
+		
+		String error = webDriver.findElement(By.className("error")).getText();
+
+		assertEquals(error,"所感の長さが最大値(2000)を超えています");
+		
+		scrollTo("0");
+	}
+
+	@Test
+	@Order(12)
+	@DisplayName("テスト12 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：一週間の振り返りが2000文字超")
+	void test12() {
+		// TODO ここに追加
+		WebElement contents = webDriver.findElement(By.xpath("//*[@id=\"intFieldName_0\"]"));
+		WebElement understanding = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]"));
+		WebElement attainmentLev = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		WebElement Impressions = webDriver.findElement(By.xpath("//*[@id=\"content_1\"]"));
+		WebElement review = webDriver.findElement(By.xpath("//*[@id=\"content_2\"]"));
+		WebElement submitBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/form/div[3]/fieldset/div/div/button"));
+		
+		final String testWord = "test"; 
+		
+		
+		contents.clear();
+		contents.sendKeys(testWord);
+		
+		understanding.click();
+		
+		WebElement understandingLev1 = webDriver.findElement(By.xpath("//*[@id=\"intFieldValue_0\"]/option[2]"));
+		
+		understandingLev1.click();
+		
+		attainmentLev.clear();
+		attainmentLev.sendKeys("1");
+		
+		getEvidence(new Object() {},"01_beforeSubmit");
+		
+		Impressions.clear();
+		Impressions.sendKeys(testWord);
+		
+		review.clear();
+		for(int i = 0; i <= 91 ; i++) {
+			review.sendKeys("あいうえおかきくけこさしすせそたちつてとなに");
+		}
+		
+		getEvidence(new Object() {},"02_beforeSubmit");
+		
+		scrollBy("1000");
+		
+		submitBtn.click();
+		
+		scrollBy("1000");
+		
+		getEvidence(new Object() {},"03_afterSubmit");
+		
+		String error = webDriver.findElement(By.className("error")).getText();
+
+		assertEquals(error,"一週間の振り返りの長さが最大値(2000)を超えています");
 	}
 
 }
