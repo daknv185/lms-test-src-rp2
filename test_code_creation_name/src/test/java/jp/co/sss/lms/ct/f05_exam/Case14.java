07c89d91f2a296a1d16472c5b9be8c4c911c3c7e
t-yoshida <daknv.its.202507@gmail.com>
2025-11-27 17:24:26 +0900

    add:Case14 受講生の試験実施機能の動作確認テストコード追加
    WebElementを利用し、受講生の試験実施機能が適切に動作するか確認するテストを作成しました。
    なおcase14では半分が正答、半分が誤答で解答するよう記述されています。
    
    以下、期待値
    テスト1: ログイン画面が表示される
    
    テスト2 : 画面遷移後、コース詳細画面が表示される
    
    テスト3 : 画面遷移後、セクション詳細画面が表示される。
    
    テスト4 : 画面遷移後、試験開始画面が表示される。
    
    テスト5 : 画面遷移後、試験問題画面が表示される。
    
    テスト6 : 画面遷移後、試験解答確認画面が表示される。また、回答数が「12／12」であることを確認できる。
    
    テスト7 : 画面遷移後、試験結果画面が表示される。「あなたのスコア : 50.0点」かつ「正答数 : 6／12」であることを確認できる。
    
    テスト8 : 画面遷移後、試験開始画面が表示される。また、「過去の試験結果」エリアに当該試験の結果が反映されている。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f05_exam/Case14.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f05_exam/Case14.java
@@ -1,6 +1,8 @@
 package jp.co.sss.lms.ct.f05_exam;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Date;
 
@@ -11,6 +13,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 試験実施機能
@@ -18,7 +23,7 @@ import org.junit.jupiter.api.TestMethodOrder;
  * @author holy
  */
 @TestMethodOrder(OrderAnnotation.class)
-@DisplayName("ケース13 受講生 試験の実施 結果50点")
+@DisplayName("ケース14 受講生 試験の実施 結果50点")
 public class Case14 {
 
 	/** テスト07およびテスト08 試験実施日時 */
@@ -41,6 +46,12 @@ public class Case14 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/");
 	}
 
 	@Test
@@ -48,6 +59,28 @@ public class Case14 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+		String lmsId = "StudentAA01";
+		String lmsPass = "StudentAA0";
+
+		loginId.clear();
+		loginId.sendKeys(lmsId);
+		password.clear();
+		password.sendKeys(lmsPass);
+
+		getEvidence(new Object() {
+		}, "01_beforeLogin");
+
+		login.click();
+
+		getEvidence(new Object() {
+		}, "02_afterLogin");
+
+		String screenInfo = webDriver.findElement(By.className("active")).getText();
+
+		assertTrue(screenInfo.contains("コース詳細"));
 	}
 
 	@Test
@@ -55,6 +88,15 @@ public class Case14 {
 	@DisplayName("テスト03 「試験有」の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		WebElement record = webDriver.findElement(By.xpath("//table//tr[td//span[text()='試験有']]"));
+
+		record.findElement(By.cssSelector("input[type='submit']")).click();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/section/detail");
 	}
 
 	@Test
@@ -62,6 +104,16 @@ public class Case14 {
 	@DisplayName("テスト04 「本日の試験」エリアの「詳細」ボタンを押下し試験開始画面に遷移")
 	void test04() {
 		// TODO ここに追加
+		WebElement detailBtn = webDriver
+				.findElement(By.xpath("//*[@id=\"sectionDetail\"]/table[1]/tbody/tr[2]/td[2]/form/input[1]"));
+
+		detailBtn.click();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/exam/start");
 	}
 
 	@Test
@@ -69,13 +121,81 @@ public class Case14 {
 	@DisplayName("テスト05 「試験を開始する」ボタンを押下し試験問題画面に遷移")
 	void test05() {
 		// TODO ここに追加
+		WebElement startTestBtn = webDriver
+				.findElement(By.xpath("//*[@id=\"main\"]/div/form/input[4]"));
+
+		startTestBtn.click();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/exam/question");
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 正答と誤答が半々で「確認画面へ進む」ボタンを押下し試験回答確認画面に遷移")
-	void test06() {
+	void test06() throws InterruptedException {
 		// TODO ここに追加
+		WebElement gonnaConfirmationBtn = webDriver
+				.findElement(By.xpath("//*[@id=\"examQuestionForm\"]/div[13]/fieldset/input"));
+		//第１問
+		WebElement answer1 = webDriver.findElement(By.xpath("//*[@id=\"answer-0-2\"]"));
+		answer1.click();
+		scrollBy("400");
+		//第２問
+		WebElement answer2 = webDriver.findElement(By.xpath("//*[@id=\"answer-1-2\"]"));
+		answer2.click();
+		scrollBy("400");
+		//第３問
+		WebElement answer3 = webDriver.findElement(By.xpath("//*[@id=\"answer-2-0\"]"));
+		answer3.click();
+		scrollBy("400");
+		//第４問
+		WebElement answer4 = webDriver.findElement(By.xpath("//*[@id=\"answer-3-0\"]"));
+		answer4.click();
+		scrollBy("400");
+		//第５問
+		WebElement answer5 = webDriver.findElement(By.xpath("//*[@id=\"answer-4-1\"]"));
+		answer5.click();
+		scrollBy("400");
+		//第６問
+		WebElement answer6 = webDriver.findElement(By.xpath("//*[@id=\"answer-5-1\"]"));
+		answer6.click();
+		scrollBy("400");
+		//第7問
+		WebElement answer7 = webDriver.findElement(By.xpath("//*[@id=\"answer-6-0\"]"));
+		answer7.click();
+		scrollBy("350");
+		//第8問
+		WebElement answer8 = webDriver.findElement(By.xpath("//*[@id=\"answer-7-0\"]"));
+		answer8.click();
+		scrollBy("350");
+		//第9問
+		WebElement answer9 = webDriver.findElement(By.xpath("//*[@id=\"answer-8-0\"]"));
+		answer9.click();
+		scrollBy("350");
+		//第10問
+		WebElement answer10 = webDriver.findElement(By.xpath("//*[@id=\"answer-9-0\"]"));
+		answer10.click();
+		scrollBy("350");
+		//第11門
+		WebElement answer11 = webDriver.findElement(By.xpath("//*[@id=\"answer-10-0\"]"));
+		answer11.click();
+		scrollBy("350");
+		//第12問
+		WebElement answer12 = webDriver.findElement(By.xpath("//*[@id=\"answer-11-0\"]"));
+		answer12.click();
+		scrollBy("1000");
+
+		gonnaConfirmationBtn.click();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/exam/answerCheck");
 	}
 
 	@Test
@@ -83,6 +203,23 @@ public class Case14 {
 	@DisplayName("テスト07 「回答を送信する」ボタンを押下し試験結果画面に遷移")
 	void test07() throws InterruptedException {
 		// TODO ここに追加
+		scrollBy("5000");
+
+		WebElement sendAnswerBtn = webDriver
+				.findElement(By.xpath("//*[@id=\"sendButton\"]"));
+
+		Thread.sleep(3500);
+
+		sendAnswerBtn.click();
+
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/exam/result");
 	}
 
 	@Test
@@ -90,6 +227,18 @@ public class Case14 {
 	@DisplayName("テスト08 「戻る」ボタンを押下し試験開始画面に遷移後当該試験の結果が反映される")
 	void test08() {
 		// TODO ここに追加
+		scrollBy("5000");
+
+		WebElement backBtn = webDriver
+				.findElement(By.xpath("//*[@id=\"examBeing\"]/div[13]/fieldset/form/input[1]"));
+
+		backBtn.click();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/exam/start");
 	}
 
 }
