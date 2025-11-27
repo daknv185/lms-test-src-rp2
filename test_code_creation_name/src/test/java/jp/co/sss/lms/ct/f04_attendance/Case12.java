d226c494d5393c339d88c3096678f81ee7fb48fc
t-yoshida <daknv.its.202507@gmail.com>
2025-11-27 13:04:22 +0900

    add:Case12 受講生の勤怠登録機能のバリデーション動作確認テスト追加
    WebElementを利用し、勤怠登録機能の入力チェックが適切に動作するか確認するテストを作成しました。
    
    以下、期待値
    テスト1: ログイン画面が表示される
    
    テスト2 : 画面遷移後、コース詳細画面が表示される
    
    テスト3 : 画面遷移後、勤怠管理画面が表示される。
    
    テスト4 : 画面遷移後、勤怠情報直接編集画面が表示される
    
    テスト5 : 画面左上「勤怠管理」の下部に
    「* 出勤時間が正しく入力されていません。」
    「* 退勤時間が正しく入力されていません。」
    とエラーメッセージが出力される
    
    テスト6 : 画面左上「勤怠管理」の下部に
    「* 出勤情報がないため退勤情報を入力出来ません。」
    とエラーメッセージが出力される
    
    テスト7 : 画面左上「勤怠管理」の下部に
    「* 退勤時刻は出勤時刻より後でなければいけません。」
    とエラーメッセージが出力される
    
    テスト8 : 画面左上「勤怠管理」の下部に
    「* 中抜け時間が勤務時間を超えています。」
    とエラーメッセージが出力される
    
    テスト9 : 画面左上「勤怠管理」の下部に
    「* 備考の長さが最大値(100)を超えています。」
    とエラーメッセージが出力される

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case12.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case12.java
@@ -1,6 +1,10 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +13,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -36,6 +44,12 @@ public class Case12 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/");
 	}
 
 	@Test
@@ -43,6 +57,28 @@ public class Case12 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+		String lmsId = "StudentAA01";
+		String lmsPass = "StudentAA0";
+
+		loginId.clear();
+		loginId.sendKeys(lmsId);
+		password.clear();
+		password.sendKeys(lmsPass);
+
+		getEvidence(new Object() {
+		}, "01_beforeLogin");
+
+		login.click();
+
+		getEvidence(new Object() {
+		}, "02_afterLogin");
+
+		String screenInfo = webDriver.findElement(By.className("active")).getText();
+
+		assertTrue(screenInfo.contains("コース詳細"));
 	}
 
 	@Test
@@ -50,6 +86,24 @@ public class Case12 {
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		visibilityTimeout(By.xpath("//*[@id=\"nav-content\"]/ul[1]/li[3]/a"), 5);
+		WebElement attendance = webDriver.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[1]/li[3]/a"));
+
+		attendance.click();
+
+		try {
+			Alert alert = webDriver.switchTo().alert();
+			alert.accept();
+		} catch (NoAlertPresentException ignore) {
+
+		}
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/attendance/detail");
+
 	}
 
 	@Test
@@ -57,6 +111,16 @@ public class Case12 {
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
 		// TODO ここに追加
+		visibilityTimeout(By.xpath("//*[@id=\"main\"]/div[1]/p/a"), 5);
+		WebElement editAttendanceLink = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/p/a"));
+
+		editAttendanceLink.click();
+
+		getEvidence(new Object() {
+		});
+
+		String url = webDriver.getCurrentUrl();
+		assertEquals(url, "http://localhost:8080/lms/attendance/update");
 	}
 
 	@Test
@@ -64,13 +128,94 @@ public class Case12 {
 	@DisplayName("テスト05 不適切な内容で修正してエラー表示：出退勤の（時）と（分）のいずれかが空白")
 	void test05() {
 		// TODO ここに追加
-	}
+		WebElement startHourForm = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]"));
+		WebElement startMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]"));
+		WebElement endHourForm = webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]"));
+		WebElement endMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]"));
+		WebElement updateBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/div/input"));
+		
+		startHourForm.click();
+		WebElement startNineHour = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]/option[11]"));
+		startNineHour.click();
+		
+		startMinuteForm.click();
+		WebElement startMinuteBlank = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]/option[1]"));
+		startMinuteBlank.click();
+		
+		endHourForm.click();
+		WebElement endHourBlank = webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]/option[1]"));
+		endHourBlank.click();
+		
+		endMinuteForm.click();
+		WebElement endZeroMinute = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]/option[2]"));
+		endZeroMinute.click();
+		
+		getEvidence(new Object() {},"01_inputted");
+		
+		scrollBy("1000");
+		
+		updateBtn.click();
+		
+		
+		
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+		
+		
+		getEvidence(new Object() {},"02_submitted");
+		
+		List<WebElement> error = webDriver.findElements(By.className("error"));
+		String errorStart = error.get(0).getText();
+		String errorEnd = error.get(1).getText();
+		
+		assertEquals("* 出勤時間が正しく入力されていません。",errorStart);
+		assertEquals("* 退勤時間が正しく入力されていません。",errorEnd);
+		}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 不適切な内容で修正してエラー表示：出勤が空白で退勤に入力あり")
 	void test06() {
 		// TODO ここに追加
+		WebElement startHourForm = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]"));
+		WebElement startMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]"));
+		WebElement endHourForm = webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]"));
+		WebElement endMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]"));
+		WebElement updateBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/div/input"));
+		
+		startHourForm.click();
+		WebElement startHourBlank = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]/option[1]"));
+		startHourBlank.click();
+		
+		startMinuteForm.click();
+		WebElement startMinuteBlank = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]/option[1]"));
+		startMinuteBlank.click();
+		
+		endHourForm.click();
+		WebElement endHourTen= webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]/option[12]"));
+		endHourTen.click();
+		
+		endMinuteForm.click();
+		WebElement endZeroMinute = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]/option[2]"));
+		endZeroMinute.click();
+		
+		getEvidence(new Object() {},"01_inputted");
+		
+		scrollBy("1000");
+		
+		updateBtn.click();
+		
+		
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+		
+		getEvidence(new Object() {},"02_submitted");
+		
+		WebElement error = webDriver.findElement(By.className("error"));
+		
+		String errorStart = error.getText();
+		
+		assertEquals("* 出勤情報がないため退勤情報を入力出来ません。",errorStart);
 	}
 
 	@Test
@@ -78,6 +223,45 @@ public class Case12 {
 	@DisplayName("テスト07 不適切な内容で修正してエラー表示：出勤が退勤よりも遅い時間")
 	void test07() {
 		// TODO ここに追加
+		WebElement startHourForm = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]"));
+		WebElement startMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]"));
+		WebElement endHourForm = webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]"));
+		WebElement endMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]"));
+		WebElement updateBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/div/input"));
+		
+		startHourForm.click();
+		WebElement startHourTen = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]/option[12]"));
+		startHourTen.click();
+		
+		startMinuteForm.click();
+		WebElement startMinuteZero = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]/option[2]"));
+		startMinuteZero.click();
+		
+		endHourForm.click();
+		WebElement endHourNine= webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]/option[11]"));
+		endHourNine.click();
+		
+		endMinuteForm.click();
+		WebElement endMinuteZero = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]/option[2]"));
+		endMinuteZero.click();
+		
+		getEvidence(new Object() {},"01_inputted");
+		
+		scrollBy("1000");
+		
+		updateBtn.click();
+		
+		
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+		
+		getEvidence(new Object() {},"02_submitted");
+		
+		WebElement error = webDriver.findElement(By.className("error"));
+		
+		String errorText = error.getText();
+		
+		assertEquals("* 退勤時刻は出勤時刻より後でなければいけません。",errorText);
 	}
 
 	@Test
@@ -85,6 +269,55 @@ public class Case12 {
 	@DisplayName("テスト08 不適切な内容で修正してエラー表示：出退勤時間を超える中抜け時間")
 	void test08() {
 		// TODO ここに追加
+		WebElement startHourForm = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]"));
+		WebElement startMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]"));
+		WebElement endHourForm = webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]"));
+		WebElement endMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]"));
+		WebElement blankTimeForm = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/table/tbody/tr[1]/td[10]/select"));
+		WebElement updateBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/div/input"));
+		
+		startHourForm.click();
+		
+		WebElement startHourNine = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]/option[11]"));
+		startHourNine.click();
+		
+		startMinuteForm.click();
+		
+		WebElement startMinuteZero = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]/option[2]"));
+		startMinuteZero.click();
+		
+		endHourForm.click();
+		
+		WebElement endHourTen = webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]/option[12]"));
+		endHourTen.click();
+		
+		endMinuteForm.click();
+		
+		WebElement endMinuteZero = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]/option[2]"));
+		endMinuteZero.click();
+		
+		blankTimeForm.click();
+		
+		WebElement blankHourFive = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/table/tbody/tr[1]/td[10]/select/option[21]"));
+		blankHourFive.click();
+		
+		getEvidence(new Object() {},"01_inputted");
+		
+		scrollBy("1000");
+		
+		updateBtn.click();
+		
+		
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+		
+		getEvidence(new Object() {},"02_submitted");
+		
+		WebElement error = webDriver.findElement(By.className("error"));
+		
+		String errorText = error.getText();
+		
+		assertEquals("* 中抜け時間が勤務時間を超えています。",errorText);
 	}
 
 	@Test
@@ -92,6 +325,63 @@ public class Case12 {
 	@DisplayName("テスト09 不適切な内容で修正してエラー表示：備考が100文字超")
 	void test09() {
 		// TODO ここに追加
+		WebElement startHourForm = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]"));
+		WebElement startMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]"));
+		WebElement endHourForm = webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]"));
+		WebElement endMinuteForm = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]"));
+		WebElement blankTimeForm = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/table/tbody/tr[1]/td[10]/select"));
+		WebElement noteForm = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/table/tbody/tr[1]/td[12]/input"));
+		WebElement updateBtn = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/div/input"));
+		String testWord = "あいうえおか";
+		
+		startHourForm.click();
+		
+		WebElement startHourNine = webDriver.findElement(By.xpath("//*[@id=\"startHour0\"]/option[11]"));
+		startHourNine.click();
+		
+		startMinuteForm.click();
+		
+		WebElement startMinuteZero = webDriver.findElement(By.xpath("//*[@id=\"startMinute0\"]/option[2]"));
+		startMinuteZero.click();
+		
+		endHourForm.click();
+		
+		WebElement endHourTen = webDriver.findElement(By.xpath("//*[@id=\"endHour0\"]/option[12]"));
+		endHourTen.click();
+		
+		endMinuteForm.click();
+		
+		WebElement endMinuteZero = webDriver.findElement(By.xpath("//*[@id=\"endMinute0\"]/option[2]"));
+		endMinuteZero.click();
+		
+		blankTimeForm.click();
+		
+		WebElement blankHourBlank = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div/div/form/table/tbody/tr[1]/td[10]/select/option[1]"));
+		blankHourBlank.click();
+		
+		noteForm.clear();
+		
+		for(int i = 0;i < 17; i++) {
+			noteForm.sendKeys(testWord);
+		}
+		
+		getEvidence(new Object() {},"01_inputted");
+		
+		scrollBy("1000");
+		
+		updateBtn.click();
+		
+		
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+		
+		getEvidence(new Object() {},"02_submitted");
+		
+		WebElement error = webDriver.findElement(By.className("error"));
+		
+		String errorText = error.getText();
+		
+		assertEquals("* 備考の長さが最大値(100)を超えています。",errorText);
 	}
 
 }
